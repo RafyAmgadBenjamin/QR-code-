293550e0592d3c9230324543ab1f1ad0e6ac67b2
Rafy <rafy.amgadbenjamin@gmail.com>
2020-01-26 19:43:11 +0200

    just adding comments

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,11 +1,13 @@
 import qrcode
 import csv
 
+
 def read_sheet_names():
-    names_index =1
+    names_index = 1
     names = []
-    with open('/home/rafy/Downloads/POC QR code/names.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    # the path from which i read the names file
+    with open("/home/rafy/Downloads/POC QR code/names.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
@@ -17,15 +19,16 @@ def read_sheet_names():
                 name = row[names_index].lower()
                 names.append(name.strip())
                 line_count += 1
-        print(f'Processed {line_count} lines.')
+        print(f"Processed {line_count} lines.")
     return names
+
 
 def qr_code_generator(name):
     qr = qrcode.QRCode()
     qr.add_data(name)
     qr.make()
     img = qr.make_image()
-    img.save('/home/rafy/Downloads/POC QR code/codes_generatged/{}.png'.format(name))
+    img.save("/home/rafy/Downloads/POC QR code/codes_generatged/{}.png".format(name))
 
 
 names = read_sheet_names()
